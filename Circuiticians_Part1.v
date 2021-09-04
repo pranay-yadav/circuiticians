0e98e2e809dc716afb987dba57004116d407c14b
pyadav1218 <pranayyadav1218@gmail.com>
2021-09-04 18:18:47 -0500

    Initial verilog skeleton code.

---
+++ Circuiticians_Part1.v
@@ -0,0 +1,46 @@
+module breadboard	();  
+
+  
+
+endmodule                           
+
+
+module testbench();
+
+  // Main code goes here
+  
+endmodule 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Dr. Becker's cheat sheet of what is wrong in the code.
+//The loop control variable can never reach 16, it is only 4 bits. Add another bit
+//There needs to be a time dealy, such a #5, inside the for loop
