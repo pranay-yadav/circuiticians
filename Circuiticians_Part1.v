5a4acf36f1cbfbf027e1ff45bd369a52708669bf
pranay-yadav <pranay.yadav@utdallas.edu>
2021-09-10 17:08:00 -0500

    Implemented functions f6-f9 in breadboard module.

--- Circuiticians_Part1.v
+++ Circuiticians_Part1.v
@@ -1,6 +1,20 @@
-module breadboard	();  
+module breadboard	(w, x, y, z, r6, r7, r8, r9);  
+input w, x, y, z;
+output r6, r7, r8, r9;
+reg r6, r7, r8, r9;
 
+always @(w, x, y, z, r6, r7, r8, r9) begin
   
+// f6 = w'x'y + w'x'z + xy'z + wx'y'z'
+r6 = ((!w)&(!x)&y) | ((!w)&(!x)&z) | (x&(!y)&z) | (w&(!x)&(!y)&(!z));
+// f7 = wxy'z' + w'xy'z + wx'y'z + w'x'yz + w'xyz' + wx'yz'
+r7 =  (w&x&(!y)&(!z)) | ((!w)&x&(!y)&z) | (w&(!x)&(!y)&z) | ((!w)&(!x)&y&z) | ((!w)&x&y&(!z))) | (w&(!x)&y&(!z));
+// f8 = yz
+r8 = (y&z);
+// f9 = w'x'y'z + w'x'yz' + w'xy'z' + w'xyz + wxy'z + wxyz' + wx'y'z' + wx'yz
+r9 = ((!w)&(!x)&(!y)&z) | ((!w)&(!x)&y&(!z)) | ((!w)&x&(!y)&(!z)) | ((!w)&x&y&z) | (w&x&(!y)&z) | (w&x&y&(!z)) | (w&(!x)&(!y)&(!z)) | (w&(!x)&y&z);
+
+end
 
 endmodule                           
 
@@ -9,6 +23,16 @@ module testbench();
 
   // Main code goes here
   
+  // Loop
+
+  reg [3:0] i; // for looping from 0-15
+  reg a, b, c, d; // for holding the bit values of i
+
+  wire f6, f7, f8, f9; // hold function return values
+
+  
+
+
 endmodule 
 
 
