3639ea4bb601afd2c20abeac6c18488dea62d806
carlex2700 <90166999+carlex2700@users.noreply.github.com>
2021-09-12 22:33:00 -0500

    Update Minterms_Carlos.v

--- Minterms_Carlos.v
+++ Minterms_Carlos.v
@@ -1,14 +1,12 @@
-//----------------------------------------------------------------------
-module Breadboard	(w,x,y,z,r1,r2,r3);  //Module Header
-input w,x,y,z;                        //Specify inputs
-output r1,r2,r3;                         //Specify outputs
-reg r1, r2, r3;                           //Output is a memory area.
+module Breadboard	(w,x,y,z,r1,r2,r3); 
+input w,x,y,z;                        
+output r1,r2,r3;                      
+reg r1, r2, r3;                       .
 
-always @ ( w,x,y,z,r1,r2,r3) begin       //Create a set of code that works line by line 
-                                      // if the variables are used
-
-//                               //Comment for the formula
+always @ ( w,x,y,z,r1,r2,r3) begin    
+	
 r1= (w&x)|(w&z)|(x&y)|(y&z);                     //Bitwise operation of the formula
+
 r2= (y&z);
 
 r3 = (((!w)&(!x)&(!y)&(z)) | ((!w)&(!x)&(y)&(!z)) 
@@ -30,26 +28,16 @@ module testbench();
   reg  c;//Value of 2^1
   reg  d;//Value of 2^0
   
-  //A wire can hold the return of a function
   wire  f0,f8,f9;
   
-  //Modules can be either functions, or model chips. 
-  //They are instantiated like an object of a class, 
-  //with a constructor with parameters.  They are not invoked,
-  //but operate like a thread.
+ 
   Breadboard zap(a,b,c,d,f0,f8,f9);
   //FunctionOne zap2(b,c,d,a,f2);
      
 	 
-  //Initial means "start," like a Main() function.
-  //Begin denotes the start of a block of code.	
+  
   initial begin
    	
-  //$display acts like a classic C printf command.
-  $display ("|##|A|B|C|D|F0|F8|F9|");
-  $display ("|==+=+=+=+=+==|==|==|");
-  
-    //A for loop, with register i being the loop control variable.
 	for (i = 0; i < 16; i = i + 1) 
 	begin//Open the code blook of the for loop
 		a=(i/8)%2;//High bit
@@ -66,7 +54,5 @@ module testbench();
 
 	end//End of the for loop code block
  
-	#10; //A time dealy of 10 time units. Hashtag Delay
-	$finish;//A command, not unlike System.exit(0) in Java.
   end  //End the code block of the main (initial)
 
