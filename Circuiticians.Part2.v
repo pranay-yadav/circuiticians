fd78e2e1e796c8c5b83e8b862c5d7bbf6abf1cfc
pranay-yadav <pranay.yadav@utdallas.edu>
2021-10-06 14:33:11 -0500

    Implemented Dec module

--- Circuiticians.Part2.v
+++ Circuiticians.Part2.v
@@ -20,8 +20,8 @@ module FullAdder(a, b, carry, sum);
 	
 endmodule
 
-// Adder-Subtracter
-module AddSub(IN1, IN2, OP, S, CAR, OVF);
+// Adder-Subtractor
+module AddSub(IN1, IN2, M, S, CAR, OVF);
 
 endmodule
 
@@ -42,6 +42,26 @@ endmodule
 
 // Decoder
 module Dec(OP, SEL);
+	input [3:0] OP;
+	output [15:0] SEL;
+
+	// 4x16 One-hot Decoder
+	assign SEL[ 0]=~OP[3]&~OP[2]&~OP[1]&~OP[0];
+	assign SEL[ 1]=~OP[3]&~OP[2]&~OP[1]& OP[0];
+	assign SEL[ 2]=~OP[3]&~OP[2]& OP[1]&~OP[0];
+	assign SEL[ 3]=~OP[3]&~OP[2]& OP[1]& OP[0];
+	assign SEL[ 4]=~OP[3]& OP[2]&~OP[1]&~OP[0];
+	assign SEL[ 5]=~OP[3]& OP[2]&~OP[1]& OP[0];
+	assign SEL[ 6]=~OP[3]& OP[2]& OP[1]&~OP[0];
+	assign SEL[ 7]=~OP[3]& OP[2]& OP[1]& OP[0];
+	assign SEL[ 8]= OP[3]&~OP[2]&~OP[1]&~OP[0];
+	assign SEL[ 9]= OP[3]&~OP[2]&~OP[1]& OP[0];
+	assign SEL[10]= OP[3]&~OP[2]& OP[1]&~OP[0];
+	assign SEL[11]= OP[3]&~OP[2]& OP[1]& OP[0];
+	assign SEL[12]= OP[3]& OP[2]&~OP[1]&~OP[0];
+	assign SEL[13]= OP[3]& OP[2]&~OP[1]& OP[0];
+	assign SEL[14]= OP[3]& OP[2]& OP[1]&~OP[0];
+	assign SEL[15]= OP[3]& OP[2]& OP[1]& OP[0];
 
 endmodule
 
