2398a5c945efdae5097d4a55e26139f31e7f2a67
Jacob1939 <79594859+Jacob1939@users.noreply.github.com>
2021-11-15 19:27:18 -0600

    Add files via upload

---
+++ CircuiticiansSphere.v
@@ -0,0 +1,50 @@
+/* SPHERE / SA = 4* pi * r^2 / V = 4 * pi * r^3 / 3 */
+		IN = 16'b0000000100111010; // 314
+		OP = 4'b0010; // Add 0 + 314
+		#60;
+		IN = 16'b0000000001100100; // 100
+		OP = 4'b0101; // Divide 314 / 100, store in pi
+		#60;
+		PI_5 = OUT;
+		OP = 4'b1111; // RESET
+		#60;
+		
+		IN = PI_5; 
+		OP = 4'b0010; // Add pi
+		#60;
+		IN = r;
+		OP = 4'b0100; // Multiply Pi * r
+		#60;
+		IN = r;
+   		OP = 4'b0100; // Multiply Pi * r ^ 2
+		#60;
+		IN = 16'b0000000000000100; // 4
+		OP = 4'b0100; // Multiply 4 * Pi * r ^ 2
+		#60;
+		SURFACEAREA_5 = OUT;
+		OP = 4'b1111; // RESET
+		#60;
+
+        IN = PI_5; 
+		OP = 4'b0010; // Add pi
+		#60;
+		IN = r;
+		OP = 4'b0100; // Multiply Pi * r
+		#60;
+		IN = r; 
+		OP = 4'b0100; // Multiply Pi * r ^ 2
+		#60;
+		IN = r; 
+		OP = 4'b0100; // Multiply Pi * r ^ 3
+		#60;
+		IN = 16'b0000000000000100; // 4
+		OP = 4'b0100; // Multiply 4 * Pi * r ^ 3
+		#60;
+		IN = 16'b0000000000000011; // 3
+		OP = 4'b0101; // 4 * pi * r ^ 3 / 3
+		#60;
+		VOLUME_5 = OUT;
+		OP = 4'b1111; // RESET
+		#60;
+	
+	/* End SPHERE */
